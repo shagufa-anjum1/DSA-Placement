75db3980b34727e27d33fc611cbdaf559a897054
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-06-30 20:09:02 +0530

    LinkeList

---
+++ Lec9_LinkedList.java
@@ -0,0 +1,15 @@
+public class Lec9_LinkedList {
+    public static class node{
+        int data;
+        node next;
+
+        public node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
