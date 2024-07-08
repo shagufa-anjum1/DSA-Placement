59ed92dc0101db62aedb0444135d154690579b47
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-08 11:22:20 +0530

    Double LinkeList

---
+++ Lec21_Double_LinkedList.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Lec21_Double_LinkedList {
+    public class node{
+        int data;
+        node next;
+        node prev;
+
+        public node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static node head;
+    public static node tail;
+    public static int size;
+
+    public void addFirst(int data){
+        // create node
+
+        node newNode = new node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+    public  void print(){
+        node temp = head;
+        while (temp != null){
+            System.out.println(temp.data+"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args){
+        LinkedList<Integer> dll = new LinkedList<>();
+        // using double linkedlist here not this linkedlist like this
+        // DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        System.out.println(dll);
+        System.out.println(dll.size());
+
+    }
+}
