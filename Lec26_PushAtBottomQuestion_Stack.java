7530bb4481c76ce488ffdd777c45dc7a842e92e8
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-09 17:38:13 +0530

    Push at Bottom Qestion of Stack

---
+++ Lec26_PushAtBottomQuestion_Stack.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class Lec26_PushAtBottomQuestion_Stack {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        pushAtBottom(s, 6);
+        while (!s.isEmpty()){
+            System.out.print(s.pop()+" ");
+
+        }
+
+    }
+}
