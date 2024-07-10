01f566cf9656aac2ce098892671d9dc441dda834
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-10 11:33:22 +0530

    Stock Span Problem in Stack

---
+++ Lec28_stockSpan_Stack.java
@@ -0,0 +1,19 @@
+public class Lec28_stockSpan_Stack {
+
+    public class node{
+        int data;
+        node next;
+
+        public node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static node head;
+    public static node tail;
+    public static int size;
+    public static void main(String[] args){
+
+    }
+}
