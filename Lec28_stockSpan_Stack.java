77da420c5212865645468fda22f7a81460e05b55
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-10 15:30:27 +0530

    Commit

--- Lec28_stockSpan_Stack.java
+++ Lec28_stockSpan_Stack.java
@@ -1,19 +1,35 @@
+import java.util.Stack;
+
 public class Lec28_stockSpan_Stack {
 
-    public class node{
-        int data;
-        node next;
+   public static void stackSpan(int stocks[], int span[]){
+       Stack<Integer> s = new Stack<>();
+       span[0] = 1;
+       s.push(0);
 
-        public node(int data){
-            this.data = data;
-            this.next = null;
+       for(int i=1; i<stocks.length; i++){
+           int currPrice = stocks[i];
 
-        }
-    }
-    public static node head;
-    public static node tail;
-    public static int size;
-    public static void main(String[] args){
+           while (!s.isEmpty() && currPrice > stocks.[s.peek()]){
+               s.pop();
+           }
+           if(s.isEmpty()){
+               span[i] = i+1;
+           }else {
+               int prevHigh = s.peek();
+               span[i] = i-prevHigh;
+           }
+           s.push(i);
+       }
+   }
 
-    }
+   public static void main(String[] args){
+       int stacks[] = {100, 80, 60, 70, 60, 85, 100};
+       int span[] ;
+       stackSpan(stacks, span);
+
+       for(int i=0; i<span.length; i++){
+           System.out.println(span[i]+" ");
+       }
+   }
 }
