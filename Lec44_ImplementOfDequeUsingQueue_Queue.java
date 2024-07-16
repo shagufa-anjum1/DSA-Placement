aa2bd4007bb198ecefd231a8832d92910c807b3a
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-16 15:51:29 +0530

    Implement of Deque using Queue

---
+++ Lec44_ImplementOfDequeUsingQueue_Queue.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Lec44_ImplementOfDequeUsingQueue_Queue {
+     static class Queue{
+        Deque<Integer> deque = new LinkedList<>();
+        // add function
+        public void add(int data){
+            deque.addLast(data);
+        }
+        // Remove Function
+        public int remove(){
+            return deque.removeFirst();
+        }
+
+        // Peek Function
+
+        public int peek(){
+            return deque.getFirst();
+        }
+
+    }
+    public static void main(String[] args){
+       Queue q = new Queue();
+       q.add(1);
+       q.add(2);
+       q.add(3);
+
+        System.out.println("Pecck : "+q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+
+    }
+}
