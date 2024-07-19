9f36b67bb40004fb4217c0b88cb5f5b16305ec05
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-19 20:39:55 +0530

    Max Length og Chain in an Greedy Algorithums

---
+++ Lec49_MaxLengthChainOfPairs_GreedyAlgorithms.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Lec49_MaxLengthChainOfPairs_GreedyAlgorithms {
+    public static void main(String[] args){
+        int pairs[][] = {{5, 24}, {39, 60}, {5, 28}, {27, 40}, {50, 90}};
+
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+        int chainLen = 1;
+        int chainEnd = pairs[0][1];  // last selected pairs
+
+        for(int i=0; i<pairs.length; i++){
+            if(pairs[i][0] > chainEnd){
+                chainLen++;
+                chainEnd = pairs[i][1];
+            }
+        }
+        System.out.println("Max Length of Pairs : "+chainLen);
+    }
+}
