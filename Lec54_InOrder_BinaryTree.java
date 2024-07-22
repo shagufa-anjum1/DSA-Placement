6828c4674ed0c58267ad9865bfef8e738d107c7d
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-22 22:17:40 +0530

    InOrder tree Traversal in an Binary Tree

---
+++ Lec54_InOrder_BinaryTree.java
@@ -0,0 +1,40 @@
+public class Lec54_InOrder_BinaryTree {
+    static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class binaryTree{
+        static int idx = -1;
+        public static node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            node newNode = new node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+        public static void inOrder(node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+    }
+    public static void main(String[] args){
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        binaryTree tree = new binaryTree();
+        node root = tree.buildTree(nodes);
+        tree.inOrder(root);
+
+    }
+}
