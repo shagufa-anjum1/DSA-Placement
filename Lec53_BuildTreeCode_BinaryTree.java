f3e36cb5edc4a753c0b00c34d62ff46a4b134f47
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-22 19:08:58 +0530

    PreOrder Tree in an Same Tree in Binary Tree

--- Lec53_BuildTreeCode_BinaryTree.java
+++ Lec53_BuildTreeCode_BinaryTree.java
@@ -27,11 +27,24 @@ public class Lec53_BuildTreeCode_BinaryTree {
             return newNode;
 
         }
+
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+
+
+        }
     }
+
     public static void main(String[] args){
        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.BuildTree(nodes);
-        System.out.println(root.data);
+        //System.out.println(root.data);
+        tree.preOrder(root);
     }
 }
