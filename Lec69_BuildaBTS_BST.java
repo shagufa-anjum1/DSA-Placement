83e1a2955b52f3f2fd1082ebba60b0d8c03f3124
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-07-26 21:20:39 +0530

    Build a BST in an Binary Search trees

---
+++ Lec69_BuildaBTS_BST.java
@@ -0,0 +1,45 @@
+public class Lec69_BuildaBTS_BST {
+     static class Node{
+         int data;
+         Node left;
+         Node right;
+         public Node(int data){
+             this.data = data;
+
+         }
+     }
+     public static Node insert(Node root, int val){
+         if(root == null){
+             root = new Node(val);
+             return root;
+         }
+
+         if(root.data > val){
+             // left subtree
+             root.left = insert(root.left, val);
+         }else {
+             // right subtree
+             root.right = insert(root.right, val);
+         }
+         return root;
+     }
+     public static void inOrder(Node root){
+         if(root == null){
+             return;
+         }
+         inOrder(root.left);
+         System.out.print(root.data+" ");
+         inOrder(root.right);
+     }
+    public static void main(String[] args){
+      int values[] = {5, 1, 3, 4, 2, 7};
+      Node root = null;
+
+      for (int i=0; i<values.length; i++){
+          root = insert(root, values[i]);
+      }
+
+      inOrder(root);
+      System.out.println();
+    }
+}
