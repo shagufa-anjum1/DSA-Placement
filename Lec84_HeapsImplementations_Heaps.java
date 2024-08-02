42464e088584b52c690c400def3f6a020440dfa9
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-08-03 01:06:57 +0530

    Implementation of Heaps in an Heaps

--- Lec84_HeapsImplementations_Heaps.java
+++ Lec84_HeapsImplementations_Heaps.java
@@ -1,5 +1,26 @@
-public class Lec84_HeapsImplementations_Heaps {
+import java.util.ArrayList;
 
+public class Lec84_HeapsImplementations_Heaps {
+  static class Heap{
+       static ArrayList<Integer> arr = new ArrayList<>();
+
+      public static void add(int data){
+          // add at last index
+          arr.add(data);
+          // x is child index
+          int x = arr.size()-1;
+          // parent index
+          int parent = (x-1)/2;
+
+          while ((arr.get(x) < arr.get(parent))){ // o(logn)
+              // swap
+              int temp = arr.get(x);
+              arr.set(x, arr.get(parent));
+              arr.set(parent, temp);
+          }
+
+      }
+  }
     public static void main(String[] args){
 
     }
