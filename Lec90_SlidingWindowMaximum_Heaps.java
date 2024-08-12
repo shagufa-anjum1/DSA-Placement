15dbe9ce0d6d4880608ace29fd89309bb1180941
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-08-12 07:46:08 +0530

    Sliding Window Maximum in an Heaps

---
+++ Lec90_SlidingWindowMaximum_Heaps.java
@@ -0,0 +1,50 @@
+import java.util.PriorityQueue;
+
+public class Lec90_SlidingWindowMaximum_Heaps {
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
+
+        public Pair(int val, int idx){
+            this.val = val;
+            this.idx = idx;
+        }
+        @Override
+        public int compareTo(Pair p2){
+            // ascending order
+            // return this.val - p2.val;
+            // decending order
+            return p2.val - this.val;
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int result[] = new int[arr.length-k+1]; // n-k+1
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for(int i=0; i<k; i++){
+            pq.add(new Pair(arr[i], i));
+        }
+
+        result[0] = pq.peek().val;
+
+        for(int i=k; i<arr.length; i++){
+            while (pq.size() > 0 && pq.peek().idx <= (i-k)){
+                pq.remove();
+            }
+            pq.add(new Pair(arr[i], i));
+            result[i-k+1] = pq.peek().val;
+        }
+
+        // print result
+
+        for(int i=0; i<result.length; i++){
+            System.out.print(result[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
