7fe2efc2cb06f86aeb25b167873a2a8b9e7748b8
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-09-19 12:15:46 +0530

    Trie in an Search

---
+++ Lec95_SearchInTrie_Trie.java
@@ -0,0 +1,53 @@
+public class Lec95_SearchInTrie_Trie {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean endOfWord = false;
+
+        public Node(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    //Insert
+    public static void insert(String word){
+        Node curr = root;
+        for(int level=0; level<word.length(); level++ ){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.endOfWord = true;
+    }
+
+    // Search
+    public static boolean search(String key){
+        Node curr = root;
+        for(int level = 0; level < key.length(); level++){
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.endOfWord == true;
+    }
+
+
+
+    public static void main(String[] args){
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
+
+    }
+}
